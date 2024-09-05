6d86c2861806d5924942d22b00ba3f37bb1b7c23
Joseph bae <111346414+Jbae04@users.noreply.github.com>
2024-09-05 01:36:31 -0700

    Updated code

---
+++ src/about/subheadingsData.js
@@ -0,0 +1,75 @@
+const subheadingsData = {
+  1: [
+    {
+      title: "About Me",
+      content: (
+        <>
+          <p>
+            I am Korean but born in california. I have been back and forth from korea to america and
+            have learned to adapt to different cultures and environments. I am a creative and curious!
+          </p>
+        </>
+      ),
+    },
+  ],
+  2: [
+    {
+      title: "Currently in college",
+      content: (
+        <>
+          <p>
+          Currently pursuing a degree in computer science at SFSU.
+            Hoping to specialize in web dev, AI, or cybersecurity.
+          </p>
+        </>
+      ),
+    },
+    {
+      title: "2026 Bachelors Degree",
+      content: (
+        <>
+          <p>
+            
+          </p>
+        </>
+      ),
+    },
+  ],
+  3: [
+    {
+      title: "2022 college student",
+      content: (
+        <>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+        </>
+      ),
+    },
+    {
+      title: "2022 Ai Scholarship",
+      content: (
+        <>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+        </>
+      ),
+    },
+    {
+      title: "2025 Intern at meta",
+      content: (
+        <>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+        </>
+      ),
+    },
+  ],
+};
+
+export default subheadingsData;
---
+++ src/skills/skillsData.js
@@ -0,0 +1,68 @@
+const skills = {
+  1: [
+    {
+      title: "HTML",
+      level: 4,
+    },
+    {
+      title: "CSS",
+      level: 4,
+    },
+    {
+      title: "SASS",
+      level: 1,
+    },
+    {
+      title: "JavaScript",
+      level: 3,
+    },
+    {
+      title: "TypeScript",
+      level: 1,
+    },
+    {
+      title: "Tailwind CSS",
+      level: 1,
+    },
+    {
+      title: "React JS",
+      level: 4,
+    },
+    {
+      title: "Figma",
+      level: 1,
+    },
+  ],
+  2: [
+    {
+      title: "PostgreSQL",
+      level: 1,
+    },
+    {
+      title: "Node.js",
+      level: 3,
+    },
+    {
+      title: "MongoDB",
+      level: 1,
+    },
+    {
+      title: "Git",
+      level: 3,
+    },
+    {
+      title: "PHP",
+      level: 1,
+    },
+    {
+      title: "SQL",
+      level: 2,
+    },
+    {
+      title: "Express.js",
+      level: 2,
+    },
+  ],
+};
+
+export default skills;
